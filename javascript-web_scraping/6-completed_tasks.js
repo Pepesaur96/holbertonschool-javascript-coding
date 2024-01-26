0c9980a1c9b06da1cdde27380ff55e1450cc9b51
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-26 11:38:47 -0400

    Task6.Compute tasks by User

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// a script that computes the number of tasks completed by user id
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const completedTask = {};
+  const tasks = JSON.parse(body);
+  for (const task of tasks) {
+    if (task.completed === true) {
+      if (completedTask[task.userId] === undefined) {
+        completedTask[task.userId] = 1;
+      } else {
+        completedTask[task.userId] += 1;
+      }
+    }
+  }
+  console.log(completedTask);
+});
