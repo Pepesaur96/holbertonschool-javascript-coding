d064e474e249745c77c567f019857c0db2bf7c75
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-26 09:59:52 -0400

    Task2.Displpay status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', res.statusCode);
+  }
+});
