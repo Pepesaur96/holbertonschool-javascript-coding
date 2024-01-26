4c36d5cc95cca424de52d21237941430d28670de
Pepesaur96 <jjvazquez96@gmail.com>
2024-01-26 01:50:03 -0400

    Task0.Console log and syntax error fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,13 +2,13 @@
 //a script that reads and prints the content of a file
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
-  }
+  } else {
   console.log(data);
+  }
 });
